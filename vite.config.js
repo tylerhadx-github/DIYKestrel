fdb3a0466b6705b9a409cebef31d82100762be24
Tyler Haddix <thaddix@raptortech.com>
2023-04-02 21:30:54 -0600

    change base

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: '/LoraText/'
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
