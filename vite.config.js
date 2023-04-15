cf48d231059731a92388de9f148cbc46ccba5639
Tyler Haddix <thaddix@raptortech.com>
2023-04-15 10:10:06 -0600

    add images

--- vite.config.js
+++ vite.config.js
@@ -22,10 +22,10 @@ export default defineConfig({
       injectRegister: 'auto',
       manifest:{
         name: 'LoraToArduino',
-        short_name: 'Lora',
+        short_name: 'LoraText',
         description: 'offline messenger',
-        theme_color: "#fff",
-        background_color: "#fff",
+        theme_color: "#4DBA87",
+        background_color: "#000000",
         display: "standalone",
         icons:[
           {
@@ -40,6 +40,28 @@ export default defineConfig({
             size: '512x512',
             type: 'image/png',
             purpose: "any"
+          },
+          {
+            "src": "./img/icons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
           }
         ]
       }
